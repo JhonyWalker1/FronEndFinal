9988431bdee31c16b3a9a6903853960fd3738832
JhonyWalker1 <jhonatanaliaga4@gmail.com>
2022-06-24 22:53:48 -0500

    final2

--- src/pages/CompraEnd/index.js
+++ src/pages/CompraEnd/index.js
@@ -6,7 +6,9 @@ const FinalView = ()=>{
         <Container>
             <Grid container spacing={3}>
                 <Grid item md={8}>
-                    <h2>Gracias por tu compra king</h2>
+                    <h2>Muchas gracias por tu compra</h2>
+                    <h3>Esperamos que tu experiencia
+                        sea gratificando</h3>
                 </Grid>
             </Grid>
         </Container>
