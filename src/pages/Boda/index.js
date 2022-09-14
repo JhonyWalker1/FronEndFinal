d10ae6d2901cc4fc750c4ee47a7823252012be2b
JhonyWalker1 <jhonatanaliaga4@gmail.com>
2022-09-14 18:11:14 -0500

    avance boda2

--- src/pages/Boda/index.js
+++ src/pages/Boda/index.js
@@ -1,4 +1,4 @@
-import { Carousel } from "react-bootstrap";
+import { Carousel,ListGroup,Table,Button  } from "react-bootstrap";
 
 import boda1 from "../../assets/boda1.jpg";
 import boda2 from "../../assets/boda2.jpg";
@@ -47,12 +47,58 @@ const BodaInvitacion = () =>{
                 </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            <h1 className="titulo">Jhonatan & Geraldine</h1>
+            <h3 className="titulo">Jhonatan</h3>
+            <h3 className="titulo">&</h3>
+            <h3 className="titulo">Geraldine</h3>
             <div className="content">
-                <h3 className="text">
-                    Familia van a venir o nada? avisen pa no guardar comida
+                <h3>
+                    Agradeceremos tu presencia en este día tan espacial para nosotros
                 </h3>
+
+                {/* <ListGroup horizontal className="my-2">
+                    <ListGroup.Item className="w-33">Viernes 11 de Noviembre </ListGroup.Item>
+                    <ListGroup.Item className="w-33">2022</ListGroup.Item>
+                    <ListGroup.Item className="w-33">6 de la tarde</ListGroup.Item>
+                </ListGroup>
+
+                <Table striped bordered hover responsive="xl">
+                    <thead>
+                        <tr>
+                            <th>Viernes 11 de Noviembre</th>
+                            <th>2022</th>
+                            <th>6 de la tarde</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Viernes 11 de Noviembre</th>
+                            <th>2022</th>
+                            <th>6 de la tarde</th>
+                        </tr>
+                    </tbody>
+                </Table> */}
+
+                <div className="contentdatos">
+                    <div>
+                        <h5>Viernes 11 de Noviembre</h5>
+                    </div>
+                    <div>
+                        <h5>2022</h5>
+                    </div>
+                    <div>
+                        <h5>6 de la tarde</h5>  
+                    </div>
+
+                </div>
+
+                <div className="direct">
+                    <h5>Te esperamos en: </h5>
+                    <Button size="lg" variant="success" href="https://www.google.com/maps/place/Jir%C3%B3n+Gral.+Luis+La+Prta+221,+Santiago+de+Surco+15038/@-12.1326311,-76.9969939,21z/data=!4m5!3m4!1s0x9105b808ee964a4b:0x4ead35aee8ab9395!8m2!3d-12.1324669!4d-76.9970234">Nuestro hogar</Button>
+                </div>
+
             </div>
+
+
 
         </div>
     )
